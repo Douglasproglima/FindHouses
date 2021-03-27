30fd2b69314c93281bcc9959d4cf41cf9ac02720
Douglas Lima <douglasproglima@gmail.com>
2021-03-27 20:21:38 -0300

    Add file dotenv example and config babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: false,
+      },
+    ],
+    [
+      'babel-plugin-root-import',
+      {
+        rootPathPrefix: '~',
+        rootPathSuffix: 'src',
+      },
+    ],
+  ],
 };
