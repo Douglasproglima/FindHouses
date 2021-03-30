cd03a369a0f1d580684243554ba0f1e5eecc5adb
Douglas Lima <douglasproglima@gmail.com>
2021-03-29 23:07:54 -0300

    New project config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
+  },
 };
