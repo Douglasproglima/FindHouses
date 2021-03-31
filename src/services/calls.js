9574f365fd0154789409504d6e9f1d278f78382d
Douglas Lima <douglasproglima@gmail.com>
2021-03-30 23:52:20 -0300

    Refactore API

--- src/services/calls.js
+++ src/services/calls.js
@@ -2,12 +2,42 @@ import api from './api';
 
 export const getHousesCall = async () => {
   try {
-    const response = await api.get(
-      'properties/v2/list-for-rent?city=Miami&state_code=FL&limit=15&offset=0&sort=relevance',
+    const result = await api.get(
+      '/properties/v2/list-for-rent?city=Miami&state_code=FL&limit=15&offset=0&sort=relevance',
     );
-    return response.data;
-  } catch (err) {
-    console.log({ err });
-    return err;
+
+    /*const result = await api.get('/properties/v2/list-for-rent', {
+      params: {
+        city: 'Miami',
+        state_code: 'FL',
+        limit: 15,
+        offset,
+        sort: 'relevance',
+      },
+    });
+
+    return result.data;*/
+
+    return result.data;
+  } catch (error) {
+    console.log(error);
+
+    return error;
+  }
+};
+
+export const getHouseDetail = async property_id => {
+  try {
+    const result = await api.get('/properties/v2/detail', {
+      params: {
+        property_id,
+      },
+    });
+
+    return result.data;
+  } catch (error) {
+    console.log(error);
+
+    return error;
   }
 };
