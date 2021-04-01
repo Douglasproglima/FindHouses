26c7c6743588655bbdf3f13fd4c8738c8cbc7302
Douglas Lima <douglasproglima@gmail.com>
2021-03-31 23:06:39 -0300

    Created New Component Loader

---
+++ src/components/molecules/Loader/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DetailText } from '../../atoms';
+import { LoaderIndicator, LoaderContainer } from './styles';
+
+export const Loader = ({ text }) => {
+  return (
+    <LoaderContainer>
+      <LoaderIndicator size="large" color="white" />
+      <DetailText>{text ? text : 'Carregando...'}</DetailText>
+    </LoaderContainer>
+  );
+};
