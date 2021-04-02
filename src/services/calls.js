09caaf9b59a10c785feb409cdf753301a13f27c1
Douglas Lima <douglasproglima@gmail.com>
2021-04-02 00:22:37 -0300

    Refactore Services

--- src/services/calls.js
+++ src/services/calls.js
@@ -1,18 +1,45 @@
 import api from './api';
 
-export const getHousesCall = async () => {
-  try {
-    const result = await api.get('/properties/v2/list-for-rent', {
-      params: {
-        city: 'Miami',
-        state_code: 'FL',
-        limit: 15,
-        offset,
-        sort: 'relevance',
-      },
-    });
+const URI_LIST = '/properties/v2/list-for-rent';
+const URI_DETAIL = '/properties/v2/detail';
 
-    return result.data;
+export const getHousesCall = async ({ query, offset }) => {
+  try {
+    if (query) {
+      const { sizeMin, sizeMax, priceMin, priceMax, bedsMin, bathsMin } = query;
+      // console.log({ query });
+      //console.log({ sizeMin, sizeMax, (priceMin && {priceMin}), priceMax, bedsMin, bathsMin });
+
+      const result = await api.get(URI_LIST, {
+        params: {
+          city: 'Miami',
+          state_code: 'FL',
+          limit: 15,
+          offset,
+          sort: 'relevance',
+          lot_sqft_min: sizeMin,
+          lot_sqft_max: sizeMax,
+          price_min: priceMin,
+          price_max: priceMax,
+          beds_min: bedsMin,
+          baths_min: bathsMin,
+        },
+      });
+      // console.log(result.data);
+      return result.data;
+    } else {
+      const result = await api.get(URI_LIST, {
+        params: {
+          city: 'Miami',
+          state_code: 'FL',
+          limit: 15,
+          offset,
+          sort: 'relevance',
+        },
+      });
+
+      return result.data;
+    }
   } catch (error) {
     console.log(error);
 
@@ -22,7 +49,7 @@ export const getHousesCall = async () => {
 
 export const getHouseDetail = async property_id => {
   try {
-    const result = await api.get('/properties/v2/detail', {
+    const result = await api.get(URI_DETAIL, {
       params: {
         property_id,
       },
